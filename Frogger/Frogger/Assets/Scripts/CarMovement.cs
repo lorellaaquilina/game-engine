e72969d0dede401071410f8ed96b81f17b5f9ca9
lorellaaquilina <33152220+lorellaaquilina@users.noreply.github.com>
2018-04-13 20:20:53 +0200

    Frog Control

---
+++ Frogger/Frogger/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+	public Rigidbody2D rb;
+
+	void FixedUpdate () 
+	{
+		rb.MovePosition (rb.position + Vector2.right * Time.fixedDeltaTime);
+
+	}
+}
