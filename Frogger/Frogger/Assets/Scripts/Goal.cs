bb16361b4cd5d257445f10b681bfd368a8f402bf
lorellaaquilina <33152220+lorellaaquilina@users.noreply.github.com>
2018-04-13 23:30:29 +0200

    Score Board

---
+++ Frogger/Frogger/Assets/Scripts/Goal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+
+	void OnTriggerEnter2D()
+	{
+		Score.CurrentScore = 100;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
---
+++ Frogger/Frogger/Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public static int CurrentScore = 0;
+
+	public Text scoreText;
+
+	void Start()
+	{
+		scoreText.text = CurrentScore.ToString ();
+	}
+}
